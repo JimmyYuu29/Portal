9dc6bc022e51cb8867202b97ec8f4f30b377a200
Jimmy <yuyihao35@hotmail.com>
2026-01-17 16:55:29 +0100

    Update app.py

--- portal/app.py
+++ portal/app.py
@@ -397,7 +397,7 @@ if __name__ == '__main__':
 
     # 启动应用
     app.run(
-        host=os.environ.get('PORTAL_HOST', '0.0.0.0'),
+        host=os.environ.get('PORTAL_HOST', '127.0.0.1'),
         port=int(os.environ.get('PORTAL_PORT', 5000)),
         debug=os.environ.get('PORTAL_DEBUG', 'false').lower() == 'true'
     )
